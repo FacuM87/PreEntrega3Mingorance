ef03031cc34c060a7fb22342861288be3cb7c691
FacuM87 <128387955+FacuM87@users.noreply.github.com>
2023-07-14 20:45:51 -0300

    incorpora no-cors

--- js/functions.js
+++ js/functions.js
@@ -4,6 +4,7 @@ let badlarReciente
 async function obtenerTasaBadlar() {
   try {
     const respuesta = await fetch("https://api.estadisticasbcra.com/tasa_badlar",{
+      mode:"no-cors",
       headers:{ 
           Authorization: "BEARER eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTk4NDk3NDMsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJrampraEBnbWFpbC5jb20ifQ.E0gSRk3el-Qq2QpjYiS2qurzdGLk3aQUZMdTPPYvkw1Jpn1GEyqnWvO9PRPKLNf7GjYIs3aC5YTTBzVDc8z9pA"
         }
